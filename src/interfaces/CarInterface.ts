be7483480d4f1f9a9e85b6505863a6e351dea4ee
Gilson Alencar <gilson.alencar94@gmail.com>
2022-07-12 17:30:34 -0300

    🐛 fix: add missing export statment

--- src/interfaces/CarInterface.ts
+++ src/interfaces/CarInterface.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 import { vehicleSchema } from './VehicleInterface';
 
-const carSchema = vehicleSchema.extend({
+export const carSchema = vehicleSchema.extend({
   doorsQty: z.number().gte(2).lte(4),
   seatsQty: z.number().gte(2).lte(7),
 });
