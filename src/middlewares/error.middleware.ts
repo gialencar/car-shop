8f8980915a93dcdf2e45f5f2aedfef5b91387f3c
Gilson Alencar <gilson.alencar94@gmail.com>
2022-07-12 22:07:28 -0300

    ✨ feat: create custom error class

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { ZodError } from 'zod';
+import CustomError from '../errors/custom.error';
 
 export default function errorMiddleware(
   err: unknown,
@@ -7,8 +7,8 @@ export default function errorMiddleware(
   res: Response,
   _next: NextFunction,
 ) {
-  if (err instanceof ZodError) {
-    console.log('--*-*-*-*-*-*-*-*-*-*-*-*-*-*-', err);
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).json({ error: err.message });
   }
 
   return res.status(500).json({ message: 'internal error' });
